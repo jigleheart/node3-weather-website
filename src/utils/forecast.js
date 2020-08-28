894c24a12f066adf176715074068bae0680ebe50
Johnathan Igleheart <johnathan.igleheart@na.ff-americas.com>
2020-08-28 13:22:48 -0400

    Added forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+
+
 const forecast = (lat, long, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=523e2d031f04cdf0ff1e50b30d3e838f&query=${lat},${long}&units=f`
     request({url, json: true}, (error, {body}) => {
@@ -9,7 +11,8 @@ const forecast = (lat, long, callback) => {
         else if(body.error){
             callback('Unable to find location.', undefined)
         } else{
-            callback(undefined, `${body.current.weather_descriptions[0]}, the temperature is ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}, the temperature is ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees. 
+            The wind is blowing ${body.current.wind_dir} at a speed of ${body.current.wind_speed}`)
         }
     })
 }
